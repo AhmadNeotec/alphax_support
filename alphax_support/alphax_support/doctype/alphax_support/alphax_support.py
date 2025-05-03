6b1d3bb3d16616d2968d39192847c303c6abd1c6
Ahmadpasha1221 <ahmadpashashaiks@gmail.com>
2025-05-03 17:44:47 +0000

    removed doctype support

--- alphax_support/alphax_support/doctype/alphax_support/alphax_support.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Neotec and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class AlphaxSupport(Document):
-	pass
